7451dba9a745201609fa411301feea03fdff799d
Ian Ker-Seymer <ian.kerseymer@shopify.com>
2024-02-01 16:56:38 -0500

    Initial customization

--- plugins/community.lua
+++ plugins/community.lua
@@ -4,6 +4,11 @@ return {
   -- example of importing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
-  -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.pack.nix" },
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.just" },
+  { import = "astrocommunity.test.neotest" },
+  { import = "astrocommunity.git.git-blame-nvim" },
 }
